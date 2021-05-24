510390bc84c860ec7e0cc975aeaa8f86e3e3e35b
Neo <choneo.ch@gmail.com>
2021-05-23 20:16:18 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'; 
+import { gql } from '@apollo/client'; 
+import { ApolloProvider } from '@apollo/client/react'; 
+import { useQuery } from '@apollo/client'; 
+
+const EXCHANGE_RATES = gql`
+	query GetExchangeRates($currency: String!) {
+		rates(currency: $currency) {
+			rate 
+		}
+	}
+`; 
+
+const RATES = gql`
+	query GetExchangeRates {
+		rates(currency: "USD"){
+			currency 
+		}
+	}
+`; 
+
+
+function Rates({onRateSelected}) {
+	const { loading, error, data } = useQuery(RATES); 
+
+	  if (loading) return <p> LOADING </p>; 
+	  if (error) return <p> ERROR: {error} </p>; 
+	
+	  return (
+		 <div className="App">
+		 <select name="rate" onChange={onRateSelected}> 
+			{
+				data.rates.map(({currency, rate}) => (
+				<option key={currency} value={rate}> 
+					{currency}
+				</option> 
+				))
+			}
+		</select> 
+		</div>
+  );	
+}
+
+function ShowRate({currency}){
+	const { loading, error, data } = useQuery(EXCHANGE_RATES, {
+		variables: { currency }, 
+	}); 
+
+	console.log(data);
+
+	if (loading) return null; 
+	if (error) return `Error! ${error}`; 
+
+	return (
+		<div> 
+			<h1> Rates Result: </h1>
+			<h1>
+				{
+					data
+				} 
+			</h1>	
+		</div>
+	);
+}
 
 function App() {
+  const [selectedCurrency, setSelectedCurrency] = useState(null); 
+
+  function onCurrencySelected({target}) {
+	console.log(target.value);
+	setSelectedCurrency(target.value); 
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	 <div className="App">
+		{selectedCurrency && <ShowRate currency={selectedCurrency} />}
+		<Rates onRateSelected={onCurrencySelected} />
+	</div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache } from '@apollo/client'; 
+import { ApolloProvider } from '@apollo/client/react'; 
+
+const client = new ApolloClient({ 
+	uri: 'https://48p1r2roz4.sse.codesandbox.io',
+	cache: new InMemoryCache()
+}); 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>,
   document.getElementById('root')
 );
 
